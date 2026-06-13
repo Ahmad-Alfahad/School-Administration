6dab5cae853b5d09860b4c80bb8eec4bfd765ac5
Ahmad-Alfahad <ahmad222alfahad@gmail.com>
2026-06-13 17:57:18 +0300

    add CRUD operations and factory for classroom

--- app/Http/Controllers/ClassroomController.php
+++ app/Http/Controllers/ClassroomController.php
@@ -2,9 +2,95 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Classroom;
+use App\Http\Requests\StoreClassroomRequest;
+use App\Http\Requests\UpdateClassroomRequest;
+use App\Services\ClassroomService;
+
 
 class ClassroomController extends Controller
 {
-    //
+    protected $classroomService;
+
+    public function __construct(ClassroomService $classroomService)
+    {
+        $this->classroomService = $classroomService;
+    }
+
+    public function index()
+    {
+        $classrooms = $this->classroomService->getAllClassroooms();
+
+        return view('classrooms.index', compact('classrooms'));
+    }
+
+    public function create()
+    {
+        return view('classrooms.create');
+    }
+
+    public function store(StoreClassroomRequest $request)
+    {
+        $this->classroomService->createClassroom(
+            $request->validated()
+        );
+
+        return redirect()
+            ->route('classrooms.index')
+            ->with(
+                'success',
+                'Classroom created successfully.'
+            );
+    }
+
+    public function show($id)
+    {
+        $classroom = $this->classroomService
+            ->getClassroomById($id);
+
+        return view(
+            'classrooms.show',
+            compact('classroom')
+        );
+    }
+
+    public function edit(Classroom $classroom)
+    {
+        return view(
+            'classrooms.edit',
+            compact('classroom')
+        );
+    }
+
+    public function update(
+        UpdateClassroomRequest $request,
+        Classroom $classroom
+    ) {
+        $this->classroomService->updateClassroom(
+            $classroom,
+            $request->validated()
+        );
+
+        return redirect()
+            ->route('classrooms.index')
+            ->with(
+                'success',
+                'Classroom updated successfully.'
+            );
+    }
+
+    public function destroy(Classroom $classroom)
+    {
+        $this->classroomService->deleteClassroom(
+            $classroom
+        );
+
+        return redirect()
+            ->route('classrooms.index')
+            ->with(
+                'success',
+                'Classroom deleted successfully.'
+            );
+    }
+
 }
